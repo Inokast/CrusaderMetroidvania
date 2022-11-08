56da4a26b83647883cf1339580b4b50d964f9029
TalynE <64740653+TalynE@users.noreply.github.com>
2022-11-08 17:13:50 -0500

    Basic Skeleton
    Started setting up basic enemy

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//Assignment/Lab/Project: Metroidvania
+//Name: Talyn Epting
+
+public enum State { idle, chasing, attacking, fleeing };
+
+public class EnemyBehavior : MonoBehaviour
+{
+    [Header("Behavior Values")]
+    GameObject player;
+    State currentState;
+    Rigidbody2D rb;
+    [SerializeField] int health;
+    [SerializeField] float speed;
+    float distanceToPlayer;
+    [SerializeField] float idleRange, chaseRange, attackRange;
+
+    public int Health
+    {
+        get { return health; }
+        set 
+        { 
+            health = value;
+            if(health < 0)
+            {
+                health = 0;
+            }
+        }
+    }
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentState = State.idle;
+        rb = GetComponent<Rigidbody2D>();
+        distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        StartCoroutine(SetState());
+    }
+
+    void Update()
+    {
+        Debug.Log(currentState);
+    }
+
+    void FixedUpdate()
+    {
+        HandleStates();
+    }
+
+    void HandleStates()
+    {
+        switch (currentState)
+        {
+            case State.idle:
+                Debug.Log("enemy is idle");
+                break;
+            case State.chasing:
+                ChasePlayer();
+                break;
+            case State.attacking:
+                AttackPlayer();
+                break;
+            default:
+                Debug.Log("error in switch statement for Enemy SetState");
+                break;
+        }
+    }
+
+    IEnumerator SetState()
+    {
+        while (true)
+        {
+            if (distanceToPlayer < idleRange && distanceToPlayer >= chaseRange)
+            {
+                currentState = State.chasing;
+            }
+            else
+            {
+                currentState = State.idle;
+            }
+
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            currentState = State.attacking;
+        }
+    }
+
+    void ChasePlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        Debug.Log("Chasing player");
+    }
+
+    void AttackPlayer()
+    {
+        rb.velocity = Vector2.zero;
+        Debug.Log("Attacking player");
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Metroidvania
+//Name: Daniel Sanchez, Andrew Krieps, Talyn Epting
+//Section: 2019SP.SGD.285.2144
+//Instructor: Aurore Locklear
+//Date: 11/7/2022
+/////////////////////////////////////////////
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
