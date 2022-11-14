cf6217e09cdc73261be8326a2f60424589e514a3
DungeonTurtle <65980438+DungeonTurtle@users.noreply.github.com>
2022-11-14 15:39:45 -0500

    UserSettingsAAAHHH

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] private float smoothSpeed;
+    private Vector3 offset = new Vector3(0,0,-10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPos = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPosition + offset;
+    }
+}
