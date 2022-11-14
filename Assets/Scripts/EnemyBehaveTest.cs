a5a01343a4d3f4095c940e4ff64419129f419a7c
TalynE <64740653+TalynE@users.noreply.github.com>
2022-11-13 21:22:41 -0500

    Skeleton Behavior Tweaking
    It continues, and will continue further-- still between testing different methods

---
+++ Assets/Scripts/EnemyBehaveTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Assignment/Lab/Project: Metroidvania
+//Name: Talyn Epting
+
+public class EnemyBehaveTest : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
