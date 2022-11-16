12adb091b60cdc239fc4896edf4041d09500ae0c
DungeonTurtle <65980438+DungeonTurtle@users.noreply.github.com>
2022-11-16 15:04:02 -0500

    Elevator

--- Assets/Scripts/ElevatorBehavior.cs
+++ Assets/Scripts/ElevatorBehavior.cs
@@ -11,30 +11,74 @@ public class ElevatorBehavior : MonoBehaviour
     private bool atB = false;
     private Vector2 posA;
     private Vector2 posB;
+    [SerializeField] private float eleSpeed = 10.0f;
+    private string goTo = "stop";
 
     // Start is called before the first frame update
     void Start()
     {
         posA = new Vector2(pointA.transform.position.x, pointA.transform.position.y);
         posB = new Vector2(pointB.transform.position.x, pointB.transform.position.y);
-
-        transform.position = posA;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(goTo != "stop")
+        {
+            if(goTo == "A")
+            {
+                atB = false;
+                Vector2 movement = Vector2.MoveTowards(transform.position, posA, eleSpeed);
+                transform.position = movement;
+                if(Vector2.Distance(movement, posA) < 0.01)
+                {
+                    goTo = "stop";
+                    atA = true;
+                    transform.position = posA;
+                }
+            }
+            if(goTo == "B")
+            {
+                atA = false;
+                Vector2 movement = Vector2.MoveTowards(transform.position, posB, eleSpeed);
+                transform.position = movement;
+                if (Vector2.Distance(movement, posB) < 0.01)
+                {
+                    goTo = "stop";
+                    atB = true;
+                    transform.position = posB;
+                }
+            }
+        }
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        Debug.Log("Collided!");
+        if (other.gameObject.tag == "Player")
         {
+            Debug.Log("Player jumped on Elevator");
             if (atA)
             {
-
+                goTo = "B";
             }
+            if (atB)
+            {
+                goTo = "A";
+            }
+        }
+    }
+
+    public void RecieveContact(string destination)
+    {
+        if (atA && destination == "B")
+        {
+            goTo = "A";
+        }
+        else if (atB && destination == "A")
+        {
+            goTo = "B";
         }
     }
 }
