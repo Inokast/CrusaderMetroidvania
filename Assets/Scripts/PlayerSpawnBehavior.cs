7c099b2cd5c27aa508786f75f9999cdc758b11ed
Inokast <65980438+DungeonTurtle@users.noreply.github.com>
2022-11-21 15:27:10 -0500

    LevelTwo

--- Assets/Scripts/PlayerSpawnBehavior.cs
+++ Assets/Scripts/PlayerSpawnBehavior.cs
@@ -10,37 +10,46 @@ using UnityEngine;
 //Date: 11/7/2022
 /////////////////////////////////////////////
 
+/* Desc.
+ * This class manages spawning the player in a new level
+ * 
+ * How to use:
+ * Add script to empty game object named "PlayerSpawnManager"
+ * Put in empty game objects where you want the player to spawn and give them the "PlayerSpawner" tag
+ * Rename any "PlayerSpawner" objects to the correlating previous level with "From" added to the beggining. Ex. "FromCaveRoomOne"
+ * 
+ */ 
+
 public class PlayerSpawnBehavior : MonoBehaviour
 {
-    [SerializeField] private GameObject[] PlayerSpawns;
     [SerializeField] GameManager gm;
     [SerializeField] GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    //This Script is called by the GameManager script to place the player at a point relative to the previous level.
     public void PlacePlayer(string lastScene)
     {
         GameObject spawnPoint;
         try
         {
+            //Attempt to find the correlating spawn point and set the spawnPoint GO equal to that point
             spawnPoint = GameObject.Find("From" + lastScene);
+            Instantiate(player, spawnPoint.transform.position, Quaternion.identity);
         }
         catch
         {
-            spawnPoint = GameObject.Find("DefaultSpawn");
+            //If a proper spawn point can't be found, try to spawn them at a default location
+            try
+            {
+                spawnPoint = GameObject.Find("DefaultSpawn");
+                Instantiate(player, spawnPoint.transform.position, Quaternion.identity);
+            }
+            catch
+            {
+                //If no spawnpoint can be found give an error message
+                Debug.Log("No suitable spawn points located. Please create a \"DefaultSpawn\" object and a \"From" + lastScene + "\" object");
+            }
             //NoSpawnpointFound
+            Debug.Log("No proper spawn points located. Please create a \"From" + lastScene + "\" object");
         }
-
-        Instantiate(player, spawnPoint.transform.position, Quaternion.identity);
     }
 }
